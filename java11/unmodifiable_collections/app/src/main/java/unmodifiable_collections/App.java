28e9033331069718d9f8ed6ebc9efdc571cf9ea5
Daehyun Kim <hatemogi@gmail.com>
2023-11-07 11:30:32 +0900

    스트림에서 불변 컬렉션 만드는 예제 추가

--- java11/unmodifiable_collections/app/src/main/java/unmodifiable_collections/App.java
+++ java11/unmodifiable_collections/app/src/main/java/unmodifiable_collections/App.java
@@ -1,11 +1,15 @@
 package unmodifiable_collections;
 
 import java.util.*;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
 
 import static java.util.Map.entry;
 
 public class App {
-    private void unmodifiableList() {
+    void unmodifiableList() {
         /* JDK8 */
         {
             List<String> stringList = Arrays.asList("가", "나", "다");
@@ -17,7 +21,7 @@ public class App {
         }
     }
 
-    private void unmodifiableSet() {
+    void unmodifiableSet() {
         /* JDK8 */
         {
             Set<String> stringSet = new HashSet<>(Arrays.asList("가", "나", "다"));
@@ -29,7 +33,7 @@ public class App {
         }
     }
 
-    private void unmodifiableMap() {
+    void unmodifiableMap() {
         /* JDK8 */
         {
             Map<String, Integer> stringMap = new HashMap<>();
@@ -41,12 +45,12 @@ public class App {
         }
         /* JDK9 */
         {
-            Map<String, Integer> stringMap = Map.of("가", 1, "나", 2, "다", 3);
+            var stringMap = Map.of("가", 1, "나", 2, "다", 3);
             System.out.println(stringMap);
         }
         /* JDK9. 10개 키값쌍이 이상인 경우 */
         {
-            Map<String, Integer> stringMap = Map.ofEntries(
+            var stringMap = Map.ofEntries(
                     entry("가", 1),
                     entry("나", 2),
                     entry("다", 3)
@@ -55,14 +59,38 @@ public class App {
         }
     }
 
-    private void unmodifiableCollectionFromStream() {
-
+    Stream<Integer> range(int endInclusive) {
+        return IntStream.rangeClosed(1, endInclusive).boxed();
     }
-    
-    public void run() {
+
+    void unmodifiableCollectionFromStream() {
+        /* toUnmodifiableList */ {
+            var evenList = range(10)
+                    .filter((var n) -> n % 2 == 0)
+                    .collect(Collectors.toUnmodifiableList());
+            System.out.println(evenList);
+        }
+
+        /* toUnmodifiableSet */ {
+            var evenStream = range(9).filter((var n) -> n % 2 == 0);
+            var tripleStream = range(9).filter((var n) -> n % 3 == 0);
+            var evenAndTriples = Stream.concat(evenStream, tripleStream)
+                    .collect(Collectors.toUnmodifiableSet());
+            System.out.println(evenAndTriples);
+        }
+
+        /* toUnmodifiableMap */ {
+            var doubles = range(3)
+                    .collect(Collectors.toUnmodifiableMap(Function.identity(), (var n) -> n * 2));
+            System.out.println(doubles);
+        }
+    }
+
+    void run() {
         unmodifiableList();
         unmodifiableSet();
         unmodifiableMap();
+        unmodifiableCollectionFromStream();
     }
 
     public static void main(String[] args) {
