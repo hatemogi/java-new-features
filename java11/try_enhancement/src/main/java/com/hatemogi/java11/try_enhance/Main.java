d1895ef40906805f02f8d5ad81e72efff3a3092a
Daehyun Kim <hatemogi@gmail.com>
2023-11-07 19:08:59 +0900

    TryResource 예제 추가

--- java11/try_enhancement/src/main/java/com/hatemogi/java11/try_enhance/Main.java
+++
@@ -1,7 +0,0 @@
-package com.hatemogi.java11.try_enhance;
-
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("헬로 월드");
-    }
-}
---
+++ java11/try_enhancement/src/main/java/try_enhancement/Main.java
@@ -0,0 +1,41 @@
+package try_enhancement;
+
+import java.io.*;
+import java.util.function.Consumer;
+
+public class Main {
+
+    static Consumer<String> lineWriter(BufferedWriter writer) {
+        return (String line) -> {
+            try {
+                writer.write(line);
+                writer.write("\n");
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        };
+    }
+
+    static void previousTryResource() throws IOException {
+        try (
+                BufferedReader r = new BufferedReader(new FileReader("build.gradle.kts"));
+                BufferedWriter w = new BufferedWriter(new FileWriter("build.gradle.copied.kts"))) {
+            w.write("// 처음 5줄을 복사합니다.\n\n");
+            r.lines().limit(5).forEach(lineWriter(w));
+        }
+    }
+
+    static void enhancedTryResource() throws IOException {
+        BufferedReader r = new BufferedReader(new FileReader("build.gradle.kts"));
+        BufferedWriter w = new BufferedWriter(new FileWriter("build.gradle.copied.kts"));
+        try (r; w) {
+            w.write("// 처음 5줄을 복사합니다.\n\n");
+            r.lines().limit(5).forEach(lineWriter(w));
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        previousTryResource();
+        enhancedTryResource();
+    }
+}
