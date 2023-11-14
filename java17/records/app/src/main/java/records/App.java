481cda686c05b9f5ee2fddcf25e582262c6fc620
Daehyun Kim <hatemogi@gmail.com>
2023-11-14 16:23:58 +0900

    Records 예제 프로젝트 준비

---
+++ java17/records/app/src/main/java/records/App.java
@@ -0,0 +1,11 @@
+package records;
+
+/**
+ * Java 17 LTS 기준 Text Blocks
+ * hhttps://docs.oracle.com/en/java/javase/14/language/records.html
+ */
+public class App {
+
+    public static void main(String[] args) {
+    }
+}
