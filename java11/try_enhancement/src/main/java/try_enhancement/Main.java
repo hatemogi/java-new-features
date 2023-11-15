5f756121d9ed2087303816469e49bdc7c26bdcb8
Daehyun Kim <hatemogi@gmail.com>
2023-11-15 09:35:07 +0900

    try-resource 이전 방식 예제 추가

--- java11/try_enhancement/src/main/java/try_enhancement/Main.java
+++ java11/try_enhancement/src/main/java/try_enhancement/Main.java
@@ -16,6 +16,21 @@ public class Main {
         };
     }
 
+    static void plainResource() throws IOException {
+        BufferedReader r = new BufferedReader(new FileReader("build.gradle.kts"));
+        try {
+            BufferedWriter w = new BufferedWriter(new FileWriter("build.gradle.copied.kts"));
+            try {
+                w.write("// 처음 5줄을 복사합니다.\n\n");
+                r.lines().limit(5).forEach(lineWriter(w));
+            } finally {
+                w.close();
+            }
+        } finally {
+            r.close();
+        }
+    }
+    
     static void previousTryResource() throws IOException {
         try (
                 BufferedReader r = new BufferedReader(new FileReader("build.gradle.kts"));
